1d311112a33ffafcabca2457208dafeafcb0e5ac
FelipeSoft <felipedecastrolima2@gmail.com>
2022-11-27 21:38:03 -0300

    Refatorando o projeto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
+window.onscroll = () => {
+    window.scrollY > 0
+    ? document.querySelector('ul.nav.nav-pills').style.height = "105px"
+    : document.querySelector('ul.nav.nav-pills').style.height = "130px";
+}
+
 function getCountDown(){
     let now = new Date();
-    let firstMatch = new Date(2022, 11, 20, 16, 00, 00);
+    let firstMatch = new Date(2022, 12, 18, 12, 00, 00);
     let diff = firstMatch - now;
     let format = new Date(diff);
     let d = fixZero(format.getDate());
@@ -8,10 +14,10 @@ function getCountDown(){
     let m = fixZero(format.getMinutes());
     let s = fixZero(format.getSeconds());
 
-    document.querySelector('.countdown--home-day').innerHTML = d;
-    document.querySelector('.countdown--home-hours').innerHTML = h;
-    document.querySelector('.countdown--home-minutes').innerHTML = m;
-    document.querySelector('.countdown--home-seconds').innerHTML = s;
+    document.querySelector('.countdown--home-day').innerHTML = d + 'd';
+    document.querySelector('.countdown--home-hours').innerHTML = h + 'h';
+    document.querySelector('.countdown--home-minutes').innerHTML = m + 'm';
+    document.querySelector('.countdown--home-seconds').innerHTML = s + 's';
 }
 function fixZero(time){
     return time < 10 ? `0${time}` : time;
